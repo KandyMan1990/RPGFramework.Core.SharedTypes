153070b750649a8306848524f2e3f082572ed214
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-29 21:39:48 +0000

    Move RuntimeResumeData to shared types

--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace RPGFramework.Core.SharedTypes
@@ -11,5 +12,15 @@ namespace RPGFramework.Core.SharedTypes
     {
         Task OnEnterAsync(IModuleArgs args);
         Task OnExitAsync();
+    }
+    
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct RuntimeResumeData
+    {
+        public byte ModuleId;
+        public int  Arg0;
+        public int  Arg1;
+        public int  Arg2;
+        public int  Arg3;
     }
 }
