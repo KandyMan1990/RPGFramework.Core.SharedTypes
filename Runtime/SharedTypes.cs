f683cf698f5594d4936787fcc1e24582dedd3ba8
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-04-08 23:11:16 +0100

    Add MemoryBank enum

--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -13,7 +13,7 @@ namespace RPGFramework.Core.SharedTypes
         Task OnEnterAsync(IModuleArgs args);
         Task OnExitAsync();
     }
-    
+
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct RuntimeResumeData
     {
@@ -22,5 +22,24 @@ namespace RPGFramework.Core.SharedTypes
         public int  Arg1;
         public int  Arg2;
         public int  Arg3;
+    }
+
+    /// <summary>
+    /// Where a variable will be read from/written to
+    /// </summary>
+    public enum MemoryBank
+    {
+        /// <summary>
+        /// Saved to the games save file
+        /// </summary>
+        Global = 0,
+        /// <summary>
+        /// Not saved to the games save file, but will survive for the game session, including module transitions
+        /// </summary>
+        Session = 1,
+        /// <summary>
+        /// Something that doesn't need to be saved nor persist for any length of time, should be used and discarded over 1 frame
+        /// </summary>
+        Temp = 2
     }
 }
